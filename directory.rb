0f012a614542401e519b37f866e956b154e2e3c1
Jack Gumoes <j.gumoes@gmail.com>
2021-01-29 14:03:42 +0000

    refactored code as per step 10

--- directory.rb
+++ directory.rb
@@ -23,48 +23,32 @@ class String
 end
 
 # default list of students, to make testing some stuff less painfull
-$default_students = [{name: "Bob", cohort: "october", hobby: "hobbying along", 
+$default_students = [{name: "Bob", cohort: :October, hobby: "hobbying along", 
   tallness: "about this big", mkultra: "definitely not"},
-{name: "Freddy the Murder Enthusiast", cohort: "June", hobby: "murdering", 
+{name: "Freddy the Murder Enthusiast", cohort: :June, hobby: "murdering", 
   tallness: "5 foot 7", mkultra: "nope"},
-{name: "Olaf", cohort: "Ylir", hobby: "pillaging villages and monastaries", 
+{name: "Olaf", cohort: :Ylir, hobby: "pillaging villages and monastaries", 
   tallness: "1 faomr", mkultra: "only if it pays well in mead and meat"},
-  {name: "Fidget Man", cohort: "", hobby: "being uncomfortable", 
+  {name: "Fidget Man", cohort: :November, hobby: "being uncomfortable", 
     tallness: "man sized", mkultra: "strong no"},
-    {name: "Fidget Boy", cohort: "", hobby: "whatever Fidget Man is doing", 
+    {name: "Fidget Boy", cohort: :November, hobby: "whatever Fidget Man is doing", 
       tallness: "boy sized", mkultra: "same as fidget man"},
-      {name: "Varg", cohort: "Ylir", hobby: "making bad music and terrible RPGs", 
+      {name: "Varg", cohort: :Ylir, hobby: "making bad music and terrible RPGs", 
         tallness: "viking", mkultra: "it conflicts with his neo-pagan beliefs"}]
+
+@students = []
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  # prints each student in the order they were entered
-  i = 0
-  while i < students.length
-    student = students[i]
-    if student[:mkultra]
-      mkultra_volunteer = "is a willing particapent"
-    else
-      mkultra_volunteer = "will not participate"
-    end
-    out = "#{i+1}. #{student[:name].underline} is a nember of the #{student[:cohort].to_s.underline} cohort. He is #{student[:tallness].underline} high, 
-          does #{student[:hobby].underline} for fun, and #{mkultra_volunteer.underline} in this faculties MKULTRA program"
-    puts out.center(out.length + 50)
-    i += 1
-  end
-  print_footer(students)
-end
-
-def print_cohort(students)
-  cohorts = students.map { |s| s[:cohort] }
+def print_cohort()
+  cohorts = @students.map { |s| s[:cohort] }
   cohorts.each do |cohort|
     puts "Students in the #{cohort.to_s.underline} cohort:"
     i = 0
-    students.each do |student|
+    @students.each do |student|
       if student[:cohort] == cohort
         if student[:mkultra]
           mkultra_volunteer = "is a willing particapent"
@@ -79,19 +63,15 @@ def print_cohort(students)
     end
     puts "\n"
   end
-  print_footer(students)
 end
 
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer()
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.trim
   # while the name is not empty, repeat this code
@@ -107,50 +87,56 @@ def input_students
     mkultra = gets.trim
     mkultra = true
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby, tallness: tallness, mkultra: mkultra}
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, tallness: tallness, mkultra: mkultra}
     s = ""
-    if students.count != 1 then s = "s" end
-    puts "Now we have #{students.count} student#{s}"
+    if @students.count != 1 then s = "s" end
+    puts "Now we have #{@students.count} student#{s}"
     # get another name from the user
     name = gets.trim
   end
   # return the array of students
-  students
 end
 
-def interactive_menu
-  students = []
-  loop do
-    # show the menu options
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "5. Show the defualt students"
-    puts "9. Exit"
-    selection = gets.chomp.delete "a-zA-Z "
-    puts "\n++++++++++++++++\n\n"
-    case selection
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "5. Add the defualt students"
+  puts "9. Exit"
+end
+
+def add_default_students
+  $default_students.each { |s| @students << s }
+end
+
+def process(selection)
+  puts "\n++++++++++++++++\n\n"
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print_cohort(students)
+      print_cohort()
     when "5"
-      print_cohort($default_students)
+      add_default_students()
     when "9"
       exit
     else
       puts "I don't think that was one of the options..."
-    end
-    puts "\n++++++++++++++++\n\n"
+  end
+  puts "\n++++++++++++++++\n\n"
+end
+
+def show_students
+  print_header()
+  print_cohort()
+  print_footer()
+end
+
+def interactive_menu
+  loop do
+    # show the menu options
+    print_menu()
+    process(gets.chomp.delete "a-zA-Z ")
   end
 end
 
 interactive_menu
-
-# #nothing happens until we call the methods
-# students = input_students
-# #print(students)
-# if students.size > 0
-#   print_header
-#   print_cohort(students)
-# end
